bf130c199733db3ba4e83d0e2eca337b2e7248a5
Ayele Mideksa Jima <harben8n2025@gmail.com>
2025-07-08 11:47:33 +0300

    Update urls.py

--- dashboard_project/urls.py
+++ dashboard_project/urls.py
@@ -1,8 +1,8 @@
-"""dashboard_project URL Configuration"""
-
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("", views.dashboard, name="dashboard"),
 ]
